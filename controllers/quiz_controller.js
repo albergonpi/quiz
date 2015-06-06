03bda5cee4786a096da6718c4413894a05740383
albergonpi <albergonpi@users.noreply.github.com>
2015-06-06 13:55:06 +0200

    versión 11
    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,29 +1,46 @@
 var models = require('../models/models.js');
 
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+    function(quiz){
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else {
+        next(new Error('No existe quizId=' + quizId));
+      }
+    }
+  ).catch(
+    function(error) {
+      next(error);
+    }
+  );
+}
+
 exports.show = function(req, res) {
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-    var model = {quiz: quiz};
-    res.render('quizes/show', model);
-  });
+  var model = {quiz: req.quiz};
+  res.render('quizes/show', model);
 };
 
 exports.answer = function(req, res) {
   var resp = "Incorrecto";
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-    if(req.query.respuesta === quiz.respuesta)
-      resp = "Correcto";
+  if(req.query.respuesta === req.quiz.respuesta)
+    resp = "Correcto";
     var model = {
-      quiz: quiz,
+      quiz: req.quiz,
       respuesta: resp
-      };
+    };
     res.render('quizes/answer', model);
-  });
 };
 
 exports.index = function(req, res) {
   models.Quiz.findAll().then(
     function(quizes) {
       res.render('quizes/index', {quizes: quizes});
+    }
+  ).catch(
+    function(error) {
+      next(error);
     }
   );
 }
