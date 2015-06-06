df67af7ccd0dde4ff4b5f72fe6dc0657c9dffa4c
albergonpi <albergonpi@users.noreply.github.com>
2015-06-06 12:36:59 +0200

    Version 9
    Adaptado entorno de BBDD para Heroku

--- models/models.js
+++ models/models.js
@@ -1,14 +1,30 @@
 var path = require('path');
 
+var url      = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)/);
+var DB_NAME  = (url[6] || null);
+var user     = (url[2] || null);
+var pwd      = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var port     = (url[5] || null);
+var host     = (url[4] || null);
+var storage  = process.env.DATABASE_STORAGE;
+
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null, {
-                                                  dialect: "sqlite",
-                                                  storage: "quiz.sqlite"
-                                                }
-                                             );
+var sequelize = new Sequelize(DB_NAME, user, pwd,
+  {
+    dialect:  dialect,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,
+    omitNull: true
+  }
+);
 
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;
 
@@ -17,7 +33,7 @@ sequelize.sync().then(function() {
     if(count === 0) {
       Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){
        console.log('Base de datos inicializada');
-      }); 
+      });
     }
   });
 });
