75b0da1a052a7a70e5ce959738391031fe6ab55a
albergonpi <albergonpi@users.noreply.github.com>
2015-06-07 11:01:56 +0200

    version 15
    Modificación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,6 +22,11 @@ exports.show = function(req, res) {
   res.render('quizes/show', model);
 };
 
+exports.edit = function(req, res) {
+  var model = {quiz: req.quiz, errors: []};
+  res.render('quizes/edit', model);
+};
+
 exports.answer = function(req, res) {
   var resp = "Incorrecto";
   if(req.query.respuesta === req.quiz.respuesta)
@@ -70,6 +75,21 @@ exports.create = function(req, res){
       res.render('quizes/new', {quiz : quiz, errors: err.errors});
     } else {
       quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
+        res.redirect('/quizes');
+      });
+    }
+  });
+};
+
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(function(err){
+    if(err){
+      res.render('quizes/edit', {quiz : req.quiz, errors: err.errors});
+    } else {
+      req.quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
         res.redirect('/quizes');
       });
     }
